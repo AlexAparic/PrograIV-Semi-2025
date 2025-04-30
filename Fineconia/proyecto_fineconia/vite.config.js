2e05e7904309e0d7f4ebac8732e37169b5449081
AlexAparic <141753383+AlexAparic@users.noreply.github.com>
2025-04-29 20:39:48 -0600

    Add files via upload

---
+++ Fineconia/proyecto_fineconia/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css',
+                 'resources/css/login-registro.css', // Agrega aquí el CSS específico
+                 'resources/css/veriCodigo.css',
+                 'resources/css/VeriCorreo.css',
+                 'resources/css/Bienvenida.css',
+                 'resources/css/Secciones.css',
+                 'resources/css/Home.css',
+                'resources/css/GastosIngresos.css',
+                 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
+    ],
+});
